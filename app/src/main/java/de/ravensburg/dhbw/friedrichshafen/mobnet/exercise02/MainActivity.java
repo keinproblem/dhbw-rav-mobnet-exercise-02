0bfe32194100585a8cdd701f0cc318043d906449
keinproblem <noli.mo@unknown.xxx>
2017-11-07 23:12:22 +0100

    final release

--- app/src/main/java/de/ravensburg/dhbw/friedrichshafen/mobnet/exercise02/MainActivity.java
+++ app/src/main/java/de/ravensburg/dhbw/friedrichshafen/mobnet/exercise02/MainActivity.java
@@ -7,9 +7,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     /**
@@ -52,28 +49,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
     }
@@ -88,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
-        private static final short PAGE_COUNT = 2;
+        private static final short PAGE_COUNT = 1;
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -96,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            Toast.makeText(MainActivity.this, "Current pos: " + position, Toast.LENGTH_SHORT).show();
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
             if (position == 0) {
@@ -104,10 +78,11 @@ public class MainActivity extends AppCompatActivity {
                 //forceUpdateWifiFragment();
                 return wifiInfoData;
             }
+            /*
             if (position == 1) {
                 WifiBroadcastChatFragment wifiBroadcastChatFragment = WifiBroadcastChatFragment.newInstance();
                 return wifiBroadcastChatFragment;
-            }
+            }*/
             return WifiInfoFragment.newInstance();
         }
 
